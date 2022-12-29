05ce604be0edf8f3da65079d786f316000f5fad4
simaoppcastro <simaoppcastro@gmail.com>
2022-12-29 17:05:56 +0000

    microservices first commit

---
+++ mslearn-dotnetmicroservices/backend/PizzaInfo.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace backend
+{
+    // model
+    public class PizzaInfo
+    {
+        public string PizzaName { get; set; }
+
+        public int Cost { get; set; }
+
+        public string Ingredients { get; set; }
+
+        public string InStock { get; set; }
+    }
+}
